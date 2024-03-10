81a8ed68c97252dc157d27e087242b5ad35b924e
pedro2350 <pedrohenrique70866@gmail.com>
2024-03-10 13:36:45 -0300

    feat: added code styles configs

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -5,10 +5,10 @@ const server = express();
 // Nesse meio, toda a config do servidor
 
 server.get('/', (_, res) => {
-
-    return res.send('hello-world')
+  
+  return res.send('hello-world');
 });
 
 
-export { server }
+export { server };
 
