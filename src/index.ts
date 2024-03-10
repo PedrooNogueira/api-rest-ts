b129368cf9364db9a8016f0290bcd19fafd21ac1
pedro2350 <pedrohenrique70866@gmail.com>
2024-03-10 16:46:29 -0300

    feat: environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { server } from './server/Server';
 
-server.listen(3335, () => console.log('Rodando papai!!!'));
+server.listen(process.env.PORT || 3335, () => {
+  console.log(`Rodando na porta ${process.env.PORT || 3335}`);
+});
