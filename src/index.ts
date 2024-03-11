4de29a872868212f6ed02da234c655a25ab333df
pedro2350 <pedrohenrique70866@gmail.com>
2024-03-10 23:54:47 -0300

    feat: character validation

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { server } from './server/Server';
 
 
 server.listen(process.env.PORT || 3335, () => {
-  console.log(`Rodando na porta ${process.env.PORT || 3335}`);
+  console.log(`Rodando na porta ${process.env.PORT || 3335}🔥`);
 });
--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,15 +1,34 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
 
-
+type SchemaOf<T> = yup.Schema<T>;
 
 interface ICidade {
   nome: string;
 }
 
+const bodyValidation: SchemaOf<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
-  console.log(req.body);
 
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+
+  let validateData: ICidade | undefined = undefined;
+  try {
+    validateData = await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    return res.json({
+      errors: {
+        default: yupError.message,
+      }
+    });
+  }
+
+  console.log(validateData);
 
   return res.send('Create!');
 };
