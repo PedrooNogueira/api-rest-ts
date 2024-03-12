847facf1791c0c76793563d4eeaccef7f70f13ff
pedro2350 <pedrohenrique70866@gmail.com>
2024-03-11 22:52:50 -0300

    feat: translations errors

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -10,6 +10,7 @@ interface ICidade {
 
 const bodyValidation: SchemaOf<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
 });
 
 
@@ -17,15 +18,18 @@ export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
 
   let validateData: ICidade | undefined = undefined;
   try {
-    validateData = await bodyValidation.validate(req.body);
-  } catch (error) {
-    const yupError = error as yup.ValidationError;
+    validateData = await bodyValidation.validate(req.body, {abortEarly: false});
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
 
-    return res.json({
-      errors: {
-        default: yupError.message,
-      }
+
+    yupError.inner.forEach(error => { 
+      if (error.path === undefined) return;
+      errors[error.path] = error.message;
     });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors });
   }
 
   console.log(validateData);
