6a6225fbaf184683132d4c8494781f4f01e74589
pedro2350 <pedrohenrique70866@gmail.com>
2024-03-18 23:19:01 -0300

    feat: methods getId, Delete, Update

--- src/server/controllers/cidades/index.ts
+++ src/server/controllers/cidades/index.ts
@@ -1,10 +1,14 @@
 import * as create from './Create';
 import * as getAll from './GetAll';
-
-
+import * as getById from './GetById';
+import * as DeleteById from './DeleteById';
+import * as UpdateById from './UpdateById';
 
 export const CidadesController = {
   ...create,
   ...getAll,
+  ...DeleteById,
+  ...UpdateById,
+  ...getById,
 };
 
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -6,12 +6,17 @@ import { CidadesController } from './../controllers';
 
 const router = Router();
 
+
+
 router.get('/', (_, res) => {
-  return res.send('Funcionando');
+  return res.send('Olá, DEV!');
 });
 
 router.get('/cidades', CidadesController.getAllValidation, CidadesController.getAll);
 router.post('/cidades', CidadesController.createValidation, CidadesController.create);
+router.get('/cidades/:id', CidadesController.getByIdValidation, CidadesController.getById);
+router.put('/cidades/:id', CidadesController.updateByIdValidation, CidadesController.updateById);
+router.delete('/cidades/:id', CidadesController.deleteByIdValidation, CidadesController.deleteById);
 
 
 
