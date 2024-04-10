6c31b1e394462036c4e2e6f57760b2c85ca9ee9f
PedrooNogueira <pedrohenrique70866@gmail.com>
2024-04-10 09:16:35 -0300

    feat: model cidades

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -3,13 +3,13 @@ import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
 
 import { validation } from '../../shared/middleware';
+import { ICidade } from '../../database/models';
 
 
-interface ICidade {
-  nome: string;
-}
+interface IBodyProps extends Omit<ICidade, 'id'> { }
+
 export const createValidation = validation((getSchema) => ({
-  body: getSchema<ICidade>(yup.object().shape({
+  body: getSchema<IBodyProps>(yup.object().shape({
     nome: yup.string().required().min(3),
   })),
 }));
@@ -17,7 +17,8 @@ export const createValidation = validation((getSchema) => ({
 
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
-  console.log(req.body);
+
 
   return res.status(StatusCodes.CREATED).json(1);
 };
+
